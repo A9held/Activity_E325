74a8a4fe4dbca1162edce80618a6882ec5129769
A9held <Aheld@hamilton.edu>
2024-09-12 15:48:20 +0000

    activity 2 IC/HW

---
+++ Activity2_E325.R
@@ -0,0 +1,35 @@
+#install.packages(c("dplyr","lubridate"))
+library(dplyr)
+library(lubridate)
+
+streamH = read.csv("/cloud/project/stream_gauge.csv")
+siteinfo = read.csv("/cloud/project/site_info.csv")
+
+# parse our date
+streamH$dateF = ymd_hm(streamH$datetime,
+                       tz = "America/New_York")
+
+year(streamH$dateF)
+
+#JOIN SITE INFO TO STREAM GAUGE HEIGHT
+floods = full_join(streamH, siteinfo, by ="siteID")
+
+peace = floods %>% 
+  filter(siteID == 2295637)
+
+example = floods %>%
+  filter(gheight.ft >= 10)
+plot(peace$dateF, peace$gheight.ft, type = "l")
+
+max.ht = floods %>%
+  group_by(names) %>%
+  summarise(max_ht_ft=max(gheight.ft, na.rm=TRUE),
+            mean_ft = mean(gheight.ft, na.rm=TRUE))
+
+flood_date = floods %>%
+  filter(gheight.ft >= flood.ft) %>%
+  group_by(names) %>%
+  summarise(min_date = min(dateF))
+
+
+
